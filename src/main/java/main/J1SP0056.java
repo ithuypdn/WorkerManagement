4b2d54332156723e5b7c86ceb6d8e95ec5618d51
huypd <huypd@10.12.80.204>
2023-10-10 13:53:07 +0700

    Worker Management

---
+++ src/main/java/main/J1SP0056.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import controller.WorkerManagement;
+
+/**
+ *
+ * @author huypd
+ */
+public class J1SP0056 {
+
+    public static void main(String[] args) {
+        new WorkerManagement().run();
+    }
+}
